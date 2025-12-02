efbb0eb1ec671f072a1aea123a2809a532a52b30
Joe <Joe.Scott-Hartley@Opencastsoftware.com>
2025-12-02 11:28:20 +0000

    chore: move dayOne run out of main
    Signed-off-by: Joe <Joe.Scott-Hartley@Opencastsoftware.com>

--- src/main/java/adventofcode/Main.java
+++ src/main/java/adventofcode/Main.java
@@ -4,11 +4,20 @@ import adventofcode.dayOne.Rotations;
 
 public class Main {
     public static void main(String[] args) {
+        // dayOne();
+        dayTwo();
+    }
+
+    public static void dayOne() {
         Rotations rotations = new Rotations(50);
         rotations.readRotations("main", "input");
         System.out.println("The position ended up in: ");
         System.out.println(rotations.getPosition());
         System.out.println("The zero counter went to: ");
         System.out.println(rotations.getZeroCounter());
+    }
+    
+    public static void dayTwo() {
+        System.out.println("Hello world");
     }
 }
