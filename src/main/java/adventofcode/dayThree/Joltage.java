c841a1d785aca070b406358dc14a7f841965de81
Joe <Joe.Scott-Hartley@Opencastsoftware.com>
2025-12-03 10:42:14 +0000

    refactor: switch to constructor
    Signed-off-by: Joe <Joe.Scott-Hartley@Opencastsoftware.com>

--- src/main/java/adventofcode/dayThree/Joltage.java
+++ src/main/java/adventofcode/dayThree/Joltage.java
@@ -5,8 +5,15 @@ import java.util.Collections;
 import java.util.List;
 
 public class Joltage {
+    int totalJoltage;
 
-    public static List<Integer> splitIntIntoDigits(String inputString, int sectionSize) {
+    public Joltage() {}
+
+    public int getTotalJoltage() {
+        return totalJoltage;
+    }
+
+    public List<Integer> splitIntIntoDigits(String inputString, int sectionSize) {
         List<Integer> sections = new ArrayList<>();
         for (int i = 0; i < inputString.length(); i += sectionSize) {
             String digit = inputString.substring(i, Math.min(i + sectionSize, inputString.length()));
@@ -15,7 +22,7 @@ public class Joltage {
         return sections;
     }
 
-    public static int findJoltage(String joltageRatings) {
+    public void findJoltage(String joltageRatings) {
         List<Integer> joltageDigits = splitIntIntoDigits(joltageRatings, 1);
         int max = joltageDigits.stream().mapToInt(x -> x).max().getAsInt();
         // protecting against max being the last digit
@@ -25,14 +32,14 @@ public class Joltage {
             if (indexOfMax + 1 == joltageDigits.size()) {
                 List<Integer> afterMax = joltageDigits.subList(0, joltageDigits.size() - 1);
                 int nextMax = afterMax.stream().mapToInt(x -> x).max().getAsInt();
-                return (nextMax * 10) + max;
+                totalJoltage += ((nextMax * 10) + max);
             } else {
                 List<Integer> afterMax = joltageDigits.subList(indexOfMax + 1, joltageDigits.size());
                 int nextMax = afterMax.stream().mapToInt(x -> x).max().getAsInt();
-                return (max * 10) + nextMax;
+                totalJoltage += ((max * 10) + nextMax);
             }
         } else {
-            return (max * 10) + max;
+            totalJoltage += ((max * 10) + max);
         }
     }
 }
--- src/test/java/dayThree/JoltageTest.java
+++ src/test/java/dayThree/JoltageTest.java
@@ -4,17 +4,23 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.List;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import adventofcode.dayThree.Joltage;
 
 public class JoltageTest {
+    Joltage joltage;
+    @BeforeEach
+    void setUp() {
+        joltage = new Joltage();
+    }
 
     @Test
     public void canSplitIntIntoDigits() {
         int sectionSize = 1;
         String startingString = "012";
-        List<Integer> sections = Joltage.splitIntIntoDigits(startingString,sectionSize);
+        List<Integer> sections = joltage.splitIntIntoDigits(startingString,sectionSize);
         assertEquals(0, sections.get(0));
         assertEquals(1, sections.get(1));
         assertEquals(2, sections.get(2));
@@ -22,15 +28,15 @@ public class JoltageTest {
 
     @Test
     public void canFindMaxJoltageWhenInOrder() {
-        int result = Joltage.findJoltage("987654321");
+        joltage.findJoltage("987654321");
         int expectedResult = 98;
-        assertEquals(expectedResult, result);
+        assertEquals(expectedResult, joltage.getTotalJoltage());
     }
     
     @Test
     public void canFindMaxJoltageWhenOutOfOrder() {
-        int result = Joltage.findJoltage("811111111111119");
+        joltage.findJoltage("811111111111119");
         int expectedResult = 89;
-        assertEquals(expectedResult, result);
+        assertEquals(expectedResult, joltage.getTotalJoltage());
     }
 }
