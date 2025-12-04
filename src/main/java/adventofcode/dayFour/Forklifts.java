d210cd36ae8db35162a06805f90a5e4e13941573
Joe <Joe.Scott-Hartley@Opencastsoftware.com>
2025-12-04 12:10:00 +0000

    feat: add case for string lists
    Signed-off-by: Joe <Joe.Scott-Hartley@Opencastsoftware.com>

--- src/main/java/adventofcode/dayFour/Forklifts.java
+++ src/main/java/adventofcode/dayFour/Forklifts.java
@@ -8,7 +8,7 @@ public class Forklifts {
         int xAxis;
         int yAxis;
         boolean isPaper;
-        
+
         public Position(int xAxis, int yAxis, boolean isPaper) {
             this.xAxis = xAxis;
             this.yAxis = yAxis;
@@ -18,29 +18,32 @@ public class Forklifts {
         public int getX() {
             return this.xAxis;
         }
-        
+
         public int getY() {
             return this.yAxis;
         }
-        
+
         public boolean getValue() {
             return this.isPaper;
         }
     }
 
     public List<Position> grid = new ArrayList<>();
-    
+
     public List<Position> getPosition() {
         return this.grid;
     }
 
+    public Forklifts() {
+    };
 
-    public Forklifts() {};
-
-    public void applyGrid(String input) {
-        for (int i = 0; i < input.length(); i++) {
-            boolean isPaper = (input.charAt(i) == '@');
-            grid.add(new Position(i+1, 1, isPaper));
+    public void applyGrid(List<String> input) {
+        for (int yIndex = 0; yIndex < input.size(); yIndex++) {
+            String line = input.get(yIndex);
+            for (int xIndex = 0; xIndex < line.length(); xIndex++) {
+                boolean isPaper = (line.charAt(xIndex) == '@');
+                grid.add(new Position(xIndex + 1, yIndex + 1, isPaper));
+            }
         }
     }
 }
--- src/test/java/dayFour/ForkliftsTest.java
+++ src/test/java/dayFour/ForkliftsTest.java
@@ -2,6 +2,9 @@ package dayFour;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -18,12 +21,14 @@ public class ForkliftsTest {
 
     @Test
     public void canApplyPositionWithinGrid() {
+        List<String> strings = new ArrayList<>();
         String input = ".";
+        strings.add(input);
         int expectedX = 1;
         int expectedY = 1;
         boolean expectedValue = false;
         Position expectedGridPosition = forklifts.new Position(expectedX, expectedY, expectedValue);
-        forklifts.applyGrid(input);
+        forklifts.applyGrid(strings);
         assertEquals(forklifts.grid.get(0).getX(), expectedGridPosition.getX());
         assertEquals(forklifts.grid.get(0).getY(), expectedGridPosition.getY());
         assertEquals(forklifts.grid.get(0).getValue(), expectedGridPosition.getValue());
@@ -31,12 +36,14 @@ public class ForkliftsTest {
     
     @Test
     public void canApplyPositionWithinGridForPaper() {
+        List<String> strings = new ArrayList<>();
         String input = "@";
+        strings.add(input);
         int expectedX = 1;
         int expectedY = 1;
         boolean expectedValue = true;
         Position expectedGridPosition = forklifts.new Position(expectedX, expectedY, expectedValue);
-        forklifts.applyGrid(input);
+        forklifts.applyGrid(strings);
         assertEquals(forklifts.grid.get(0).getX(), expectedGridPosition.getX());
         assertEquals(forklifts.grid.get(0).getY(), expectedGridPosition.getY());
         assertEquals(forklifts.grid.get(0).getValue(), expectedGridPosition.getValue());
@@ -44,15 +51,41 @@ public class ForkliftsTest {
 
     @Test
     public void canApplyPositionWithinGridForLongerString() {
+        List<String> strings = new ArrayList<>();
         String input = ".@";
+        strings.add(input);
         Position expectedFirstGridPosition = forklifts.new Position(1, 1, false);
         Position expectedSecondGridPosition = forklifts.new Position(2, 1, true);
-        forklifts.applyGrid(input);
+        forklifts.applyGrid(strings);
         assertEquals(forklifts.grid.get(0).getX(), expectedFirstGridPosition.getX());
         assertEquals(forklifts.grid.get(0).getY(), expectedFirstGridPosition.getY());
         assertEquals(forklifts.grid.get(0).getValue(), expectedFirstGridPosition.getValue());
         assertEquals(forklifts.grid.get(1).getX(), expectedSecondGridPosition.getX());
         assertEquals(forklifts.grid.get(1).getY(), expectedSecondGridPosition.getY());
         assertEquals(forklifts.grid.get(1).getValue(), expectedSecondGridPosition.getValue());
+    }
+    
+    @Test
+    public void canApplyPositionWithinGridForMultipleStrings() {
+        List<String> strings = new ArrayList<>();
+        strings.add(".@");
+        strings.add("@.");
+        Position expectedOneOnePosition = forklifts.new Position(1, 1, false);
+        Position expectedTwoOnePosition = forklifts.new Position(2, 1, true);
+        Position expectedOneTwoPosition = forklifts.new Position(1, 2, true);
+        Position expectedTwoTwoPosition = forklifts.new Position(2, 2, false);
+        forklifts.applyGrid(strings);
+        assertEquals(forklifts.grid.get(0).getX(), expectedOneOnePosition.getX());
+        assertEquals(forklifts.grid.get(0).getY(), expectedOneOnePosition.getY());
+        assertEquals(forklifts.grid.get(0).getValue(), expectedOneOnePosition.getValue());
+        assertEquals(forklifts.grid.get(1).getX(), expectedTwoOnePosition.getX());
+        assertEquals(forklifts.grid.get(1).getY(), expectedTwoOnePosition.getY());
+        assertEquals(forklifts.grid.get(1).getValue(), expectedTwoOnePosition.getValue());
+        assertEquals(forklifts.grid.get(2).getX(), expectedOneTwoPosition.getX());
+        assertEquals(forklifts.grid.get(2).getY(), expectedOneTwoPosition.getY());
+        assertEquals(forklifts.grid.get(2).getValue(), expectedOneTwoPosition.getValue());
+        assertEquals(forklifts.grid.get(3).getX(), expectedTwoTwoPosition.getX());
+        assertEquals(forklifts.grid.get(3).getY(), expectedTwoTwoPosition.getY());
+        assertEquals(forklifts.grid.get(3).getValue(), expectedTwoTwoPosition.getValue());
     }
 }
