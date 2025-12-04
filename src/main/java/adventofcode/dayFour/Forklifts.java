6e276b5fbf9b12442fdfe2a25fa22a7200c067bb
Joe <Joe.Scott-Hartley@Opencastsoftware.com>
2025-12-04 11:51:09 +0000

    feat: starting test
    Signed-off-by: Joe <Joe.Scott-Hartley@Opencastsoftware.com>

---
+++ src/main/java/adventofcode/dayFour/Forklifts.java
@@ -0,0 +1,43 @@
+package adventofcode.dayFour;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Forklifts {
+    public class Position {
+        int xAxis;
+        int yAxis;
+        boolean isPaper;
+        
+        public Position(int xAxis, int yAxis, boolean isPaper) {
+            this.xAxis = xAxis;
+            this.yAxis = yAxis;
+            this.isPaper = isPaper;
+        }
+
+        public int getX() {
+            return this.xAxis;
+        }
+        
+        public int getY() {
+            return this.yAxis;
+        }
+        
+        public boolean getValue() {
+            return this.isPaper;
+        }
+    }
+
+    public List<Position> grid = new ArrayList<>();
+    
+    public List<Position> getPosition() {
+        return this.grid;
+    }
+
+
+    public Forklifts() {};
+
+    public void applyGrid(String input) {
+        grid.add(new Position(1, 1, false));
+    }
+}
---
+++ src/test/java/dayFour/ForkliftsTest.java
@@ -0,0 +1,31 @@
+package dayFour;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import adventofcode.dayFour.Forklifts;
+import adventofcode.dayFour.Forklifts.Position;
+
+public class ForkliftsTest {
+    Forklifts forklifts;
+
+    @BeforeEach
+    void setUp() {
+        forklifts = new Forklifts();
+    }
+
+    @Test
+    public void canApplyPositionWithinGrid() {
+        String input = ".";
+        int expectedX = 1;
+        int expectedY = 1;
+        boolean expectedValue = false;
+        Position expectedGridPosition = forklifts.new Position(expectedX, expectedY, expectedValue);
+        forklifts.applyGrid(input);
+        assertEquals(forklifts.grid.get(0).getX(), expectedGridPosition.getX());
+        assertEquals(forklifts.grid.get(0).getY(), expectedGridPosition.getY());
+        assertEquals(forklifts.grid.get(0).getValue(), expectedGridPosition.getValue());
+    }
+}
