12038f7ef4d13f607d6e714798102f234814c1f1
Joe <Joe.Scott-Hartley@Opencastsoftware.com>
2025-12-08 10:40:50 +0000

    feat: add base case for parsing number string
    Signed-off-by: Joe <Joe.Scott-Hartley@Opencastsoftware.com>

---
+++ src/main/java/adventofcode/daySix/Cephalopod.java
@@ -0,0 +1,12 @@
+package adventofcode.daySix;
+
+public class Cephalopod {
+    public Cephalopod() {
+        
+    }
+
+    public String[] parseNumbers(String string) {
+        return string.split(" ");
+    }
+    
+}
---
+++ src/test/java/daySix/CephalopodTest.java
@@ -0,0 +1,23 @@
+package daySix;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import adventofcode.daySix.Cephalopod;
+
+public class CephalopodTest {
+    Cephalopod cephalopod;
+
+    @BeforeEach
+    void setUp() {
+        cephalopod = new Cephalopod(); 
+    }
+
+    @Test
+    public void canParseStringOfNumbers() {
+        String[] expectedArray = {"1", "2"};
+        assertArrayEquals(expectedArray, cephalopod.parseNumbers("1 2"));
+    }
+}
